74aed11e57fa9dd0cbbb88a61fe063926b814103
Anderson.goncalves <andyrgs@gmail.com>
2022-07-27 22:31:39 -0300

    ATUALIZACAO

--- Store/urls.py
+++ Store/urls.py
@@ -1,4 +1,3 @@
-
 from django.urls import path
 from Store import views 
 
@@ -6,5 +5,7 @@ from Store import views
 
 urlpatterns = [
     path('', views.index, name='index'), 
-    path('teste/', views.teste, name='teste')
+    path('departamentos/', views.departamentos, name='departamentos'),
+    path('categorias/', views.categorias, name='categorias'),
+    path('produtos/', views.produtos, name='produtos')
 ]
--- Store/views.py
+++ Store/views.py
@@ -1,7 +1,8 @@
+from unicodedata import category
 from django.shortcuts import render
 from django.http import HttpResponse
-from Store.models import Departamento
-
+from Store.models import Categoria, Departamento, Categoria, Produto
+from Store.models import Produto
 # Create your views here.
 def index(request):
     meu_nome = 'Andy da Silva'
@@ -9,8 +10,18 @@ def index(request):
     context = {'nome': meu_nome,'artigo':'o' if sexo == 'M' else 'a'}
     return render(request,'index.html', context)
 
-def teste(request):
+def departamentos(request):
   #  depto = ['Casa','Informatica','Telefonia']
     depto = Departamento.objects.all()
     context = {'departamentos': depto}
-    return render(request,'teste.html', context)
+    return render(request,'departamentos.html', context)
+
+def categorias(request):
+     listas_categorias = Categoria.objects.all()
+     context = {'categorias': listas_categorias}
+     return render(request,'categorias.html', context)
+
+def produtos(request):
+     listas_produtos = produtos.objects.all()
+     context = {'produtos': listas_produtos}
+     return render(request,'produtos.html', context)
