a9ac880b520029ab76bc0c9541389cd391c2f467
Anderson.goncalves <andyrgs@gmail.com>
2022-08-10 22:15:34 -0300

    atualizacao

--- Store/views.py
+++ Store/views.py
@@ -1,9 +1,11 @@
+import email
 from multiprocessing import context
 from unicodedata import category
 from django.shortcuts import render
 from django.http import HttpResponse
 from Store.models import Categoria, Departamento, Categoria, Produto
 from Store.models import Produto
+from django.core.mail import send_mail
 # Create your views here.
 def index(request):
     meu_nome = 'Andy da Silva'
@@ -43,5 +45,25 @@ def produto_detalhe(request, id):
 def institucional (request):
     return render(request, 'institucional.html')    
 
-def contato(resquest):
-  return render(request, 'contato.html')   
+def contato(request):
+    return render(request, 'contato.html')   
+
+def enviar_email(request):
+    nome= request.POST ['Nome']  
+    telefone = request.POST ['telefone']
+    assunto = request.POST ['Assunto']
+    mensagem = request.POST ['mensagem']
+    remetente = request.POST ['email']
+    destinatario = ['andyrgs@gmail.com']
+
+    corpo = f"Nome: {nome} \nTelefone: {telefone} \nMensagem: <br>\
+    Telefone: {telefone} <br>\
+    Mensagem: {mensagem}"
+
+    try:
+        send_mail(assunto, corpo, remetente, destinatario)
+        context = {'msg': 'E-mail enviado com sucesso!'} 
+    except: 
+        context = {'msg': 'Erro ao eviar e-mail!'} 
+
+    return render(request,'contato.html', context)
